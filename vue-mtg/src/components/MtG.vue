5f59ceba01c10cabf7c9bacedb39a135e689f881
denzow <denzow@gmail.com>
2018-12-16 00:25:12 +0900

    [add] 大まかなマークアップ

--- vue-mtg/src/components/MtG.vue
+++ vue-mtg/src/components/MtG.vue
@@ -1,8 +1,24 @@
 <template>
   <div class="mtg">
     <div class="card-base">
-      <div class="card-inner">
-
+      <div class="card-background">
+        <div class="card-inner">
+          <div class="card-header">
+            <span>対抗呪文</span>
+            <span>青青</span>
+          </div>
+          <div class="card-picture">
+          </div>
+          <div class="card-category">
+            <span>インスタント</span>
+            <span>scouty</span>
+          </div>
+          <div class="card-text-container">
+            <div class="card-text">
+              呪文１つを対象とし、それを打ち消す。
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -25,16 +41,52 @@ export default {
 
 .card-base {
   width: 203px;
-  height: 271px;
-  border: solid 10px #000;
+  height: 280px;
+  border-width: 10px 10px 20px;
+  border-color: #000;
+  border-style: solid;
   border-radius: 15px;
-  background-color: #5599EE;
-  padding: 5px 5px 15px;
+  background-color: #000;
 }
 
+.card-background {
+  border-radius: 4px 4px 15px 15px;
+  background-color: #00AAFF;
+  padding: 5px 5px 5px;
+  height: 260px;
+}
 .card-inner {
-  background-color: red;
-  height: 100%;
+  height: 275px;
   width: 100%;
+  display: flex;
+  flex-direction: column;
+}
+
+.card-category,
+.card-header {
+  height: 13px;
+  background-color: #AACCFF;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  font-size: 8px;
+  padding: 2px;
+}
+
+.card-picture {
+  height: 142px;
+  background-color: black;
+}
+
+.card-text-container {
+  height: 100px;
+  background-color: #EEEEFF;
+  font-size: 9px;
+  display: table;
+
+  .card-text {
+    display: table-cell;
+    vertical-align: middle;
+  }
 }
 </style>
