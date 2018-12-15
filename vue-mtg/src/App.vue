1745e607159f230fd4de8cda3803359102bd2742
denzow <denzow@gmail.com>
2018-12-15 23:08:26 +0900

    1st

---
+++ vue-mtg/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <MtG />
+  </div>
+</template>
+
+<script>
+import MtG from './components/MtG.vue'
+
+export default {
+  name: 'app',
+  components: {
+    MtG
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  width: 100%;
+  height: 100vh;
+  background-color: grey;
+  padding-top: 100px;
+}
+</style>
---
+++ vue-mtg/src/components/MtG.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="mtg">
+    <div class="card-base">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.mtg {
+  display: flex;
+  justify-content: center;
+}
+
+.card-base {
+  width: 220px;
+  height: 310px;
+  border: solid 10px #000;
+  border-radius: 15px;
+}
+</style>
